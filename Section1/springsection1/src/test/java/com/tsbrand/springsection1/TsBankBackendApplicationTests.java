dfc06e26ee95990dfbe032278bab851d6d50e0f1
Sourav2255Bho <bhowmicks2255@gmail.com>
2025-04-24 17:58:34 +0530

    Section 1 Commited

---
+++ Section1/springsection1/src/test/java/com/tsbrand/springsection1/TsBankBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tsbrand.springsection1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TsBankBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
