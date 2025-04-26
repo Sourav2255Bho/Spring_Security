e9647674d8ca99c9890d28eb3e8716f6c95a5ea5
Sourav2255Bho <bhowmicks2255@gmail.com>
2025-04-26 23:53:56 +0530

    Section 2 Commit

---
+++ Section2/springsection2/src/main/java/com/tsbrand/TsBankBackendApplication.java
@@ -0,0 +1,13 @@
+package com.tsbrand;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TsBankBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TsBankBackendApplication.class, args);
+	}
+
+}
